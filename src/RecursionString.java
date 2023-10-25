f3f2a0d34c7262c32a0e788be0183a0ef2641966
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2023-10-25 15:45:19 +0300

    Update RecursionString.java

--- src/RecursionString.java
+++ src/RecursionString.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class RecursionString {
+public class RecursionString {
+    public static String printString(String str){
+        String res = "";
+        if (str.isEmpty()){
+            return "";
+        } else{
+            char first = str.charAt(0);
+            String next = printString(str.substring(1));
+            if(str.length()==1){
+                return first+"";
+            }else {
+                return res = first+next;
+            }
+        }
+    }
 }
+
