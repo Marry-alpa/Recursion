90356a3eb2134ac0114fd369746e7598dcc3b31d
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2023-10-25 15:45:33 +0300

    Update ReversingString.java

--- src/ReversingString.java
+++ src/ReversingString.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class ReversivgString {
+public class ReversingString {
+    public static String reverse(String str){
+        String right;
+        String left;
+        int l = str.length();
+        if (l<=1){
+            return str;
+        }
+        left = str.substring(0, l/2);
+        right = str.substring(l/2, l);
+        return reverse(right)+ reverse(left);
+    }
 }
