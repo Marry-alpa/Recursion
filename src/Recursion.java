5cbcd499bed594639491666c228c7e702b0c5e1b
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2023-10-25 15:44:49 +0300

    Update Recursion.java

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class Recursion {
+public class Recursion {
+    public static String rec(int n){
+        if(n==1){
+            return "1";
+        }
+        return rec(n-1)+" "+n;
+    }
+    public static String recRev(int m){
+        if (m==9){
+            return "9";
+        }
+        return recRev(m+1)+" " +m;
+    }
+    public static String stackOver(String str){
+        return stackOver(str);
+    }
 }
+
