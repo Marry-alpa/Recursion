da70b875637355b71d3b420392c76636485c888b
Marry-alpa <145450328+Marry-alpa@users.noreply.github.com>
2023-10-25 15:45:04 +0300

    Update RecursionArr.java

--- src/RecursionArr.java
+++ src/RecursionArr.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class RecursionArr {
+public class RecursionArr {
+    int val[];
+    StringBuilder sb = new StringBuilder();
+
+    RecursionArr(int i) {
+        val = new int[i];
+    }
+
+    String printArr(int i) {
+        if (i == 0) {
+            return " ";
+        }else {
+            printArr(i-1);
+        }
+        String output = val[i-1]+ "\n";
+        sb.append(output);
+        return sb.toString();
+    }
+    public static int sumArr(int[] val, int i) {
+        if (i == 0) {
+            return val[0];
+        } else {
+            return val[i]+sumArr(val, i-1);
+        }
+    }
 }
+
+
